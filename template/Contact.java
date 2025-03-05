e03535897a911e2710927feb436d9d4126b210b5
JamesHaimoffFairfield <james.haimoff@student.fairfield.edu>
2025-03-05 13:43:55 -0500

    Contact progress

--- template/Contact.java
+++ template/Contact.java
@@ -1,4 +1,8 @@
 package template;
+
+import java.util.HashMap;
+import java.util.Map;
+
 //James Haimoff Push Test
 /**
  * model a Contact 
@@ -10,7 +14,24 @@ package template;
  * 
  */
 
+
+
 class Contact {
-	// public 
+    private String phoneNumber;
+    private String phoneType;
+    private String name;
+	public Contact(String name){
+        System.out.println("Intiialized Contact at these points");
+        this.name = name;
+    }
+    // public list<PhonebookEntry> getPhonebookEntries(){
+
+    // }
+    public void addPhonebookEntry(String phoneNumber, String phoneType){
+                // Map<String, Integer> ageMap = new HashMap<>();
+        this.phoneNumber = phoneNumber;
+        this.phoneType = phoneType;
+        System.out.println("Added PhoneBook Entry");
+    }
 
 }
